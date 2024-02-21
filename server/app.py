e222b8df026add084c7f003f443e5779415f18d3
baragan30 <baragan30@yahoo.com>
2024-02-21 17:26:56 +0200

    Robot Control

--- server/app.py
+++ server/app.py
@@ -1,14 +1,20 @@
+import sys;
 from flask import Flask, render_template,request,jsonify
 from flask_sock import Sock
 
 app = Flask(__name__)
 sock = Sock(app)
+list = []
+
 
 @sock.route('/echo')
 def echo(ws):
+    # list.append(ws)
+    # if len(list ) > 1:
+    #     list[0].send("hello")
     while True:
         data = ws.receive()
-        ws.send(data[::-1])
+        ws.send(data)
 
 @app.route("/get_my_ip", methods=["GET"])
 def get_my_ip():
---
+++ server/static/continous_action_button.js
@@ -0,0 +1,50 @@
+class ContinuousActionButton {
+    constructor(buttonId, triggerKey, actionFunction) {
+      this.button = document.getElementById(buttonId);
+      this.triggerKey = triggerKey;
+      this.actionFunction = actionFunction;
+      this.interval = null;
+  
+      // Bind event listeners
+      this.bindButtonEvents();
+      this.bindKeyEvents();
+    }
+  
+    startAction() {
+      if (!this.interval) {
+        this.actionFunction();
+        this.interval = setInterval(() => {
+          this.actionFunction();
+        }, 100);
+      }
+    }
+  
+    stopAction() {
+      clearInterval(this.interval);
+      this.interval = null;
+    }
+  
+    bindButtonEvents() {
+      this.button.addEventListener('mousedown', () => this.startAction());
+      this.button.addEventListener('mouseup', () => this.stopAction());
+      this.button.addEventListener('mouseleave', () => this.stopAction());
+  
+      // Optional: For touch devices
+      this.button.addEventListener('touchstart', () => this.startAction());
+      this.button.addEventListener('touchend', () => this.stopAction());
+    }
+  
+    bindKeyEvents() {
+      document.addEventListener('keydown', (event) => {
+        if (event.key === this.triggerKey) {
+          this.startAction();
+        }
+      });
+  
+      document.addEventListener('keyup', (event) => {
+        if (event.key === this.triggerKey) {
+          this.stopAction();
+        }
+      });
+    }
+  }
